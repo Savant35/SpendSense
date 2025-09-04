761a8cdabca3c4f67359b1688107ddf8a3740bd9
Ali Arthur <savant@Mac.lan>
2025-09-04 17:34:12 -0400

    added function to accept file and display csv file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-# import pandas as pd
+import pandas as pd
+
 # import plotly.express as px
 import streamlit as st
 # import json
@@ -9,7 +10,17 @@ st.set_page_config(page_title="Finace Tracker", page_icon=" ", layout="wide")
 
 
 def load_transactions(file):
-    pass
+    try:
+        df = pd.read_csv(file)
+        df.columns = [col.strip() for col in df.columns]  # removes all leading spaces
+        df["Amount"] = df["Amount"].str.replace(",", "").astype(float)
+        df["Date"] = pd.to_datetime(df["Date"], format="%d %b %Y")
+
+        st.write(df)
+        return df
+    except Exception as e:
+        st.error(f"Error proecessing file: {str(e)}")
+        return None  # no data loaded
 
 
 def main():
@@ -18,6 +29,16 @@ def main():
 
     if uploaded_file is not None:
         df = load_transactions(uploaded_file)
+
+        if df is not None:
+            debits_df = df[df["Debit/Credit"] == "Debit"].copy()
+            credits_df = df[df["Debit/Credit"] == "Debit"].copy()
+
+            tab1, tab2 = st.tabs(["Expenses (Debits)", "Payments (Credits)"])
+            with tab1:
+                st.write(debits_df)
+            with tab2:
+                st.write(credits_df)
 
 
 main()
